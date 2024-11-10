045024372a7d0544c7687f3968f537f7790a9d38
Muhammad Abdulwahid <m.abdulwahid008@gmail.com>
2024-11-10 19:24:22 +0500

    commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
-export const BASE_URL = 'http://localhost:5000/api/admin'
-// export const BASE_URL = 'https://api.imfact.io/api/admin'
+// export const BASE_URL = 'http://localhost:5000/api/admin'
+export const BASE_URL = 'https://api.imfact.io/api/admin'
 export const IMAGE_URL = 'https://images.imfact.io'
 
 
