cf3e8795dd3ac78924b47e2fd2d4ea4d8e53ff30
Usamakha99 <usamabin2580@gmail.com>
2024-12-10 19:51:37 +0500

    responsive tables

--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -7,9 +7,9 @@ import { deactivateEmployee, deleteEmployee } from '../services/employeeServices
 function EmployeeTable({ employess, setEmployees }) {
     const [t] = useTranslation("global");
     return (
-        <div className="overflow-x-auto">
-            <table className="min-w-full ">
-                <thead>
+ <div className="overflow-x-auto sm:overflow-hidden">
+            <table className="min-w-full">
+                <thead className='sm:hidden'>
                     <tr className="text-left border-y-[1px] border-themeGrey-100">
                         <th className="py-3 px-4 text-themeBlack-200 text-sm font-semibold">{t("Serial")}</th>
                         <th className="py-3 px-4 text-themeBlack-200 text-sm font-semibold">{t("name")}</th>
@@ -26,10 +26,18 @@ function EmployeeTable({ employess, setEmployees }) {
                     ))}
                 </tbody>
             </table>
-            {employess.length === 0 &&
+            {/* Fallback for smaller screens */}
+<div className="sm:flex flex-col gap-4 hidden">
+   
+{employess.map((item, index) => (
+                    <TableRow item={item} index={index} setEmployees={setEmployees}/>
+))}
+</div>
+
+            {/* {employess.length === 0 &&
                 <p className='w-full text-center text-sm text-themeBlack-200 font-medium py-3 border-themeGrey-100 border-b-[1px]'>
                     {t("nodata")}
-                </p>}
+                </p>} */}
         </div>
     )
 }
@@ -81,7 +89,8 @@ const TableRow = ({ item, index, setEmployees }) => {
 
 
     return (
-        <tr key={index} className="hover:bg-themeGrey-400 border-b-[1px] border-themeGrey-100">
+        <>
+        <tr key={index} className="sm:hidden hover:bg-themeGrey-400 border-b-[1px] border-themeGrey-100">
             <td className="py-3 px-4 text-sm text-themeBlack-200">{index + 1}</td>
             <td className="py-3 px-4 text-sm text-themeBlack-200">{item.name}</td>
             <td className="py-3 px-4 text-sm text-themeBlack-200">{item.email}</td>
@@ -108,5 +117,66 @@ const TableRow = ({ item, index, setEmployees }) => {
                 </button>
             </td>
         </tr>
+        <Link to={`/employee-detail/${item.employee_id}/${item._id}`} className=' hidden sm:flex flex-col bg-white gap-2 border-themeGrey-100 border-[1px] rounded-xl p-4' key={index}>
+
+            <div className="flex gap-1 justify-between">
+                <p className="text-xs text-themeBlack-200 mr-2">{index + 1}.</p>
+                {/* <p className="text-xs text-themeBlack-200 flex-1">#{item.employee_id}</p> */}
+                <p className={`text-xs text-white rounded-lg p-1 pt-0.5 text-right ${
+                    item.status === 'active' ? 'bg-green-500' : 'bg-red-400'
+                }`}>
+                    {item.status}
+                </p>
+            </div>
+
+            <div className="flex justify-between items-center">
+                <p className="text-xs text-themeBlack-200">{t("Name")}</p>
+                <p className="text-xs text-themeBlack-200">{item.name}</p>
+            </div>
+            <div className="flex justify-between items-center">
+                <p className="text-xs text-themeBlack-200">{t("Email")}</p>
+                <p className="text-xs text-themeBlack-200">{item.email}</p>
+            </div>
+            <div className="flex justify-between items-center">
+                <p className="text-xs text-themeBlack-200">{t("Department")}</p>
+                <p className="text-xs text-themeBlack-200 capitalize">{item.department}</p>
+            </div>
+            <div className="flex justify-between items-center">
+                <p className="text-xs text-themeBlack-200">{t("Phone")}</p>
+                <p className="text-xs text-themeBlack-200">{item.phone}</p>
+            </div>
+            
+            {/* Action Buttons */}
+            <div className="flex gap-4 mt-2 justify-between items-center">
+    <button
+        onClick={() => activateOrDeactivate(item._id)}
+        className={`w-10 h-5 rounded-full flex items-center p-1 px-1 transition-colors duration-300 ${item.status === 'active' ? 'bg-green-500' : 'bg-gray-300'}`}
+    >
+        <div
+            className={`w-4 h-4 bg-white rounded-full shadow-md transform transition-transform duration-300 ${item.status === 'active' ? 'translate-x-4' : '-translate-x-0'}`}
+        />
+    </button>
+
+    <div className="flex gap-3">
+        <Link
+            to={`/employee-edit/${item.name}/${item._id}`}
+            className="text-xs text-white bg-themePink rounded-md px-3 py-1"
+        >
+            {t("Edit")}
+        </Link>
+        <button
+            onClick={() => deleteEmploye(item._id)}
+            className="text-xs text-white bg-red-400 rounded-md px-3 py-1"
+        >
+            {t("Delete")}
+        </button>
+    </div>
+</div>
+
+
+          
+        </Link>
+
+        </>
     )
 }
--- src/components/Usertable.js
+++ src/components/Usertable.js
@@ -8,8 +8,8 @@ function Usertable({ users }) {
     const [t] = useTranslation("global");
     return (
         <div className="overflow-x-auto sm:overflow-hidden">
-            <table className="min-w-full sm:hidden">
-                <thead>
+            <table className="min-w-full">
+                <thead className='sm:hidden'>
                     <tr className="text-left border-y-[1px] border-themeGrey-100">
                         <th className="py-3 px-4 text-themeBlack-200 text-sm font-semibold">{t('Serial')}</th>
                         <th className="py-3 px-4 text-themeBlack-200 text-sm font-semibold">{t('Nickname')}</th>
@@ -27,28 +27,9 @@ function Usertable({ users }) {
             </table>
 
 
-            <div className="sm:flex flex-col gap-2 hidden">
+            <div className="sm:flex flex-col gap-4 hidden">
     {users.map((item, index) => (
-        <Link to={`/user/${item.nickname}/${item._id}`} className="flex flex-col bg-white gap-2 border-themeGrey-100 border-[1px] rounded-xl p-4">
-
-            <div className="flex gap-1 justify-between">
-                <p className="text-xs text-themeBlack-200 mr-2">{index + 1}.</p>
-                <p className="text-xs text-themeBlack-200 flex-1">{item.nickname}</p>
-                <p className={`text-xs text-white ${item.approved ? 'bg-green-500' : 'bg-gray-300'} rounded-lg p-1 pt-0.5 text-right`}>
-                    {item.approved ? t('Approved') : t('Pending')}
-                </p>
-            </div>
-
-            <div className="flex justify-between items-center">
-                <p className="text-xs text-themeBlack-200">{t('Email')}</p>
-                <p className="text-xs text-themeBlack-200">{item.email}</p>
-            </div>
-            <div className="flex justify-between items-center">
-                <p className="text-xs text-themeBlack-200">{t('Phone')}</p>
-                <p className="text-xs text-themeBlack-200">{item.phone}</p>
-            </div>
-
-        </Link>
+        <TableRow item={item} index={index} />
     ))}
 </div>
 
@@ -88,30 +69,62 @@ const TableRow = ({ item, index }) => {
         setLoading(false);
     }
     return (
-        <tr key={index} className="hover:bg-themeGrey-400 border-b-[1px] border-themeGrey-100">
-            <td className="py-3 px-4 text-sm text-themeBlack-200">{index + 1}</td>
-            <td className="py-3 px-4 text-sm text-themeBlack-200">{item.nickname}</td>
-            <td className="py-3 px-4 text-sm text-themeBlack-200">{item.email}</td>
-            <td className="py-3 px-4 text-sm text-themeBlack-200">{item.phone}</td>
-            <td className='py-3 px-4'>
-                <button
-                    onClick={approveorUnapprove}
-                    disabled={loading}
-                    className={`w-12 h-6 rounded-full disabled:cursor-not-allowed flex items-center p-1 px-1 transition-colors duration-300 ${isOn ? 'bg-green-500' : 'bg-gray-300'
+    <> 
+    <tr key={index} className="sm:hidden hover:bg-themeGrey-400 border-b-[1px] border-themeGrey-100">
+        <td className="py-3 px-4 text-sm text-themeBlack-200">{index + 1}</td>
+        <td className="py-3 px-4 text-sm text-themeBlack-200">{item.nickname}</td>
+        <td className="py-3 px-4 text-sm text-themeBlack-200">{item.email}</td>
+        <td className="py-3 px-4 text-sm text-themeBlack-200">{item.phone}</td>
+        <td className='py-3 px-4'>
+            <button
+                onClick={approveorUnapprove}
+                disabled={loading}
+                className={`w-12 h-6 rounded-full disabled:cursor-not-allowed flex items-center p-1 px-1 transition-colors duration-300 ${isOn ? 'bg-green-500' : 'bg-gray-300'
+                    }`}
+            >
+                <div
+                    className={`w-4 h-4 bg-white rounded-full shadow-md transform transition-transform duration-300 ${isOn ? 'translate-x-6' : 'translate-x-0'
                         }`}
-                >
-                    <div
-                        className={`w-4 h-4 bg-white rounded-full shadow-md transform transition-transform duration-300 ${isOn ? 'translate-x-6' : 'translate-x-0'
-                            }`}
-                    />
-                </button>
-            </td>
-            <td className='px-4 py-1.5 flex-1 flex gap-1'>
-                <Link to={`/user/${item.nickname}/${item._id}`} className='outline-none min-w-2 text-sm text-white bg-themePink rounded-md px-4 py-2'>
-                    {t('View')}
-                </Link>
-            </td>
-        </tr>
+                />
+            </button>
+        </td>
+        <td className='px-4 py-1.5 flex-1 flex gap-1'>
+            <Link to={`/user/${item.nickname}/${item._id}`} className='outline-none min-w-2 text-sm text-white bg-themePink rounded-md px-4 py-2'>
+                {t('View')}
+            </Link>
+        </td>
+    </tr>
+    <Link to={`/user/${item.nickname}/${item._id}`} className=" hidden sm:flex flex-col bg-white gap-2 border-themeGrey-100 border-[1px] rounded-xl p-4">
+
+            <div className="flex gap-1 justify-between">
+                <p className="text-xs text-themeBlack-200 mr-2">{index + 1}.</p>
+                <p className="text-xs text-themeBlack-200 flex-1">{item.nickname}</p>
+                <p className={`text-xs text-white ${item.approved ? 'bg-green-500' : 'bg-gray-300'} rounded-lg p-1 pt-0.5 text-right`}>
+                    {item.approved ? t('Approved') : t('Pending')}
+                </p>
+            </div>
+
+            <div className="flex justify-between items-center">
+                <p className="text-xs text-themeBlack-200">{t('Email')}</p>
+                <p className="text-xs text-themeBlack-200">{item.email}</p>
+            </div>
+            <div className="flex justify-between items-center">
+                <p className="text-xs text-themeBlack-200">{t('Phone')}</p>
+                <p className="text-xs text-themeBlack-200">{item.phone}</p>
+            </div>
+            <div className="flex gap-4 mt-4 justify-between items-center">
+    <button
+        onClick={() => approveorUnapprove(item._id)}
+        className={`w-10 h-5 rounded-full flex items-center p-1 px-1 transition-colors duration-300 ${item.status === 'active' ? 'bg-green-500' : 'bg-gray-300'}`}
+    >
+        <div
+            className={`w-4 h-4 bg-white rounded-full shadow-md transform transition-transform duration-300 ${item.status === 'active' ? 'translate-x-4' : '-translate-x-0'}`}
+        />
+    </button>
+    </div>
+        </Link>
+        </>
+       
     )
 }
 
